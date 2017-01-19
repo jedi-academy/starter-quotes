fd81b3be465cbd63d2ac87d749d9f80f897eb60a
Arnold Myint <blinkmonster@gmail.com>
2017-01-18 16:15:15 -0800

    Added random() that will generate a random number and only display 1 image instead of 6

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -13,6 +13,8 @@ class Welcome extends Application
 	/**
 	 * Homepage for our app
 	 */
+
+	//Original index() provided
 	public function index()
 	{
 		// this is the view we want shown
@@ -21,6 +23,27 @@ class Welcome extends Application
 		// build the list of authors, to pass on to our view
 		$source = $this->quotes->all();
 		$authors = array ();
+		foreach ($source as $record)
+		{
+			$authors[] = array ('who' => $record['who'], 'mug' => $record['mug'], 'what' => $record['what'], 'href' => $record['where']);
+		}
+		$this->data['authors'] = $authors;
+
+		$this->render();
+	}
+
+	//This will generate a random number and only provide 
+	public function random()
+	{
+		// this is the view we want shown
+		$this->data['pagebody'] = 'homepage';
+
+		// build the list of authors, to pass on to our view
+		$ranNo = rand(1,6);
+		$source = array($this->quotes->get($ranNo));
+
+		$authors = array ();
+		
 		foreach ($source as $record)
 		{
 			$authors[] = array ('who' => $record['who'], 'mug' => $record['mug'], 'what' => $record['what'], 'href' => $record['where']);
