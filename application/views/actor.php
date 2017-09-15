5fc7ebd8e72ef5aadb38de1b47891488e148ef0c
JHeggen <jonathan.heggen@gmail.com>
2017-09-14 17:46:07 -0700

    Actor.php formatting

--- application/views/actor.php
+++ application/views/actor.php
@@ -2,6 +2,8 @@
         <div class="span4">
                 <a href="/welcome/show/{key}">
                         <img src="/pix/{who}.jpg" title="{who} plays {role}"/>
+                    <br /> <span><i>{what}</i></span>
+                    <br /> <span>- {who}, <i>{role}</i></span>
                 </a>
         </div>
 </div>
--- changelog.md
+++ changelog.md
@@ -9,3 +9,4 @@ Sep 14 - Added changelog.md - Jonathan
 Sep 14 - Added new quote - Kuanysh
 Sep 14 - Added quote tooltip - Jonathan
 Sep 14 - Created new Quote Display page - Kuanysh
+Sep 14 - Updated actor page formatting - Jonathan
