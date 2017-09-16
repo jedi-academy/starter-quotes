0c192ed1775c11a22e57119dea60c116785f6af4
Shawn <sdf1342@gmail.com>
2017-09-16 16:12:16 -0700

    #formated Quotes

--- application/views/actor.php
+++ application/views/actor.php
@@ -1,7 +1,21 @@
 <div class="row">
-    <div class="span4">
-        <a href="/welcome/show/{key}">
+    <div class="span12">
+<!--        <a href="/welcome/show/{key}">-->
+        <div class="span3">
             <img src="/pix/{who}.jpg" title="{who} plays {role}"/>
-        </a>
+        </div>
+
+        <div class="span5">
+            <h3>{who}({role})</h3>
+            <br>
+            <h5 class="blink_me">{what}</h5>
+
+        </div>
+
+<!--        </a>-->
+    <div class="span12">
+        <a href="/welcome/"><button type="button" class="btn btn-primary">Back</button></a>
+
     </div>
+
 </div>
--- changelog.md
+++ changelog.md
@@ -7,6 +7,8 @@ ______________________________________________________
 3. No style and convention for now
 ______________________________________________________
 
-**SK: 2017-09-14** Adding moused over naming
-**LH: 2017-09-14** Adding Rick Sanchez avatar with quote
-**LH: 2017-09-14** Added .gitignore
+* **SK: 2017-09-14:** Adding moused over naming 
+* **LH: 2017-09-14:** Adding Rick Sanchez avatar with quote
+* **LH: 2017-09-14:** Added .gitignore
+* **SK: 2017-09-15:** Added quote display
+* **SK: 2017-09-15:** Formated Quote display
