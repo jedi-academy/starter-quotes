59d154be949c9b662d803e536ca46895da3bca90
Davin Deol <davindeol@gmail.com>
2018-01-16 17:45:26 -0800

    Added pic

--- application/models/Quotes.php
+++ application/models/Quotes.php
@@ -24,7 +24,9 @@ class Quotes extends CI_Model
 		'5'	 => array('who'	 => 'Joan Rivers', 'role'	 => 'Dot Matrix',
 			'what'	 => 'Goodbye, virgin alarm.'),
 		'6'	 => array('who'	 => 'George Wyner', 'role'	 => 'Colonel Sandurz',
-			'what'	 => 'No, sir! I didn\'t see you playing with your dolls again!')
+			'what'	 => 'No, sir! I didn\'t see you playing with your dolls again!'),
+		'7'	 => array('who'	 => 'Smash Mouth', 'role'	 => 'Best Band Ever',
+			'what'	 => 'I ain\'t the sharpest tool in the shed')
 	);
 
 	// Constructor
