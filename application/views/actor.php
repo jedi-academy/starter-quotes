6d3306b2a271f86ac02f4f1b1189cc0e94bcbc31
delan elliot <delan.elliot@gmail.com>
2018-01-18 23:19:29 +0000

    Changed formatting of the actor view
    Signed-off-by: delan elliot <delan.elliot@gmail.com>

--- application/views/actor.php
+++ application/views/actor.php
@@ -1,7 +1,10 @@
 <div class="row">
     <div class="span4">
+        <P>{who} plays {role} in the classic 1987 film Spaceballs</P>
+
         <a href="welcome/show/{key}">
-            <img src="/pix/{who}.jpg" title="{who} plays {role}"/>
+            <img src="/pix/{who}.jpg" title="{who} plays {role}" class="actorpic"/>
         </a>
+        <blockquote>"{what}"</blockquote>
     </div>
 </div>
