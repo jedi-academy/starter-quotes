3f5a58e1e6de277b3d33568cbeb2cac1f3aec71a
jade303 <tomjmckay@hotmail.ca>
2018-01-18 12:56:37 -0800

    Added tooltip and fixed broken image

--- application/models/Quotes.php
+++ application/models/Quotes.php
@@ -25,7 +25,7 @@ class Quotes extends CI_Model
 			'what'	 => 'Goodbye, virgin alarm.'),
 		'6'	 => array('who'	 => 'George Wyner', 'role'	 => 'Colonel Sandurz',
 			'what'	 => 'No, sir! I didn\'t see you playing with your dolls again!'),
-		'7'	 => array('who'	 => 'Skipper Boy', 'role'	 => 'Blurrry',
+		'7'	 => array('who'	 => 'Dark Helmet', 'role'	 => 'Blurrry',
 			'what'	 => 'Don\'t know, don\'t care.')
 	);
 
