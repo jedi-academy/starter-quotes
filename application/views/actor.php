6237b5fa640aac38fbeafab6796802d796db86b3
Wei Zhou <mike.weizhou@gmail.com>
2018-01-20 18:00:13 -0800

    Added role and quote to actor profile page

--- application/views/actor.php
+++ application/views/actor.php
@@ -2,5 +2,8 @@
         <div class="span4">
                 <img src="/pix/{who}.jpg" title="{who} plays {role}"/>
                 <br/>
+                <b>ROLE:</b> {role}
+                <br/>
+                <b>QUOTE:</b> {what}
         </div>
 </div>
