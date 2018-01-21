a30fc32daf789d5fead125ec17a23f0a332a3138
Wei Zhou <mike.weizhou@gmail.com>
2018-01-20 17:56:16 -0800

    Removed redundant link when viewing actor profile

--- application/views/actor.php
+++ application/views/actor.php
@@ -1,7 +1,6 @@
 <div class="row">
         <div class="span4">
-                <a href="welcome/show/{key}">
-                        <img src="/pix/{who}.jpg" title="{who} plays {role}"/>
-                </a>
+                <img src="/pix/{who}.jpg" title="{who} plays {role}"/>
+                <br/>
         </div>
 </div>
