bc229c90584d6c7bd84ad6b76506e3d7f1998d28
rost <rmyshkin@my.bcit.ca>
2018-01-21 12:51:09 -0800

    resolved conflicts, recommiting

---
+++ application/views/actor.php
@@ -0,0 +1,14 @@
+<div class="row">
+        <div class="span4">
+                <a href="/welcome/show/{key}">
+                        <img src="/pix/{who}.jpg" title="{who} plays {role}"/>
+                        
+                </a>
+        </div>
+    
+</div>
+<div class="row">
+    <div class="span4"> 
+        <p title="{who} plays {role}">{who} plays {role}</p>
+    </div>
+</div>
